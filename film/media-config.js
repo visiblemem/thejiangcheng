50a6e8eec39f71d95169ebd2964ba84db0155fd5
visibleyou <VISISTU01@GMAIL.COM>
2026-08-14 19:10:17 +0800

    Add Film media API configuration

---
+++ film/media-config.js
@@ -0,0 +1,5 @@
+window.JC_MEDIA = Object.freeze({
+  // One-time setup after deploying cloudflare/media-api:
+  // filmApi: 'https://jiangcheng-media-api.<your-workers-subdomain>.workers.dev/api/film'
+  filmApi: ''
+});
